d440ad2f8d417e202350019dc28dac5c3bf7ccbd
zemscky <aleksei.zemscky@yandex.ru>
2022-09-12 00:41:42 +0300

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,11 @@ public class Main {
         int age = 20;
         if (age >= 2 && age <= 6) {
             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");
-        }
-        else if (age >= 7 && age <= 18) {
+        } else if (age >= 7 && age <= 18) {
             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу");
-        }
-        else if (age > 18 && age < 24) {
+        } else if (age > 18 && age < 24) {
             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в университет");
-        }
-        else if (age > 24) {
+        } else if (age > 24) {
             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить на работу");
         }
 
@@ -21,16 +18,39 @@ public class Main {
         int childAge = 15;
         if (childAge < 5) {
             System.out.println("Если ребенку " + childAge + " лет, то он не может кататься на аттракционе");
-        }
-        else if (childAge >= 5 && childAge < 14) {
+        } else if (childAge >= 5 && childAge < 14) {
             System.out.println("Если ребенку " + childAge + " лет, то он может кататься на аттракционе только в сопровождении взрослого. Если взрослого нет, то кататься нельзя");
-        }
-        else {
+        } else {
             System.out.println("Если ребенку " + childAge + " лет, то он может кататься на аттракционе без сопровождения взрослого");
         }
 
         // Задача 3
         System.out.println("Задача 3");
+        int one = 100;
+        int two = 100;
+        int three = 100;
+        if (one > two) {
+            if (one >= three) {
+                System.out.println("Наибольшее число: " + one);
+            } else {
+                System.out.println("Наибольшее число: " + three);
+            }
+        } else if (two > one) {
+            if (two >= three) {
+                System.out.println("Наибольшее число: " + two);
+            } else {
+                System.out.println("Наибольшее число: " + three);
+            }
+        } else {
+            if (one > three) {
+                System.out.println("Наибольшее число: " + one);
+            }else if (three > one) {
+                System.out.println("Наибольшее число: " + three);
+            }
+            else {
+                System.out.println("Все 3 числа равны");
+            }
 
+        }
     }
 }
