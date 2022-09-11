af1c5530bc015a0ee051049ead6018552b6e4355
zemscky <aleksei.zemscky@yandex.ru>
2022-09-11 23:16:00 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1");
+        int age = 20;
+        if (age >= 2 && age <= 6) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");
+        }
+        else if (age >= 7 && age <= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу");
+        }
+        else if (age > 18 && age < 24) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в университет");
+        }
+        else if (age > 24) {
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить на работу");
+        }
+
+
+
+
+        // Задача 2
+        System.out.println("Задача 2");
+        // Задача 3
+        System.out.println("Задача 3");
+
+    }
+}
